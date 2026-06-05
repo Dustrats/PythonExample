ba7fa9441fea76046550d45710b326909a821739
Linus Fredriksson <linus.fredriksson@sas.com>
2026-06-05 09:16:07 +0200

    added fix to random state

--- PyEx.py
+++ PyEx.py
@@ -15,7 +15,7 @@ subset = df[["salary", "satisfaction_score", "project_completion_rate"]]
 y = df[["left_company"]]
 x = subset
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 model = LogisticRegression()
 model.fit(X_train, np.ravel(y_train))
